a0ccb201c0736a1e288715dcfebde1675c7b0315
Sanctuary-token <84331575+Sanctuary-token@users.noreply.github.com>
2021-08-04 16:53:37 +0530

    Update callsmusic.py

--- VCPlayBot/services/callsmusic/callsmusic.py
+++ VCPlayBot/services/callsmusic/callsmusic.py
@@ -10,6 +10,7 @@ pytgcalls = PyTgCalls(client)
 
 @pytgcalls.on_stream_end()
 def on_stream_end(chat_id: int) -> None:
+    print("task done"+chat_id)
     queues.task_done(chat_id)
 
     if queues.is_empty(chat_id):
